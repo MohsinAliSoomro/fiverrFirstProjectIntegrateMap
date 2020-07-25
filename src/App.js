b8e3b9fc1a635c56a22baeda2a19c4a5a1d66d6f
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-07-25 22:53:24 +0500

    get Map in browser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+  // useEffect(()=>{
+  //   fetch('https://developers.onemap.sg/commonapi/search?searchVal=revenue&returnGeom=Y&getAddrDetails=Y&pageNum=1')
+  //   .then(res=>res.json())
+  //   .then(result=>{
+  //     console.log(result)
+  //   })
+    
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
